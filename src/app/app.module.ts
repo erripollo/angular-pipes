b2fc9b62a5648672a145792d7f83824f0b3603c4
erripollo <pollone.enrico@gmail.com>
2021-11-12 11:49:55 +0100

    Pipes stringhe, valute, date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { registerLocaleData} from '@angular/common';
+import localeIt from '@angular/common/locales/it';
+registerLocaleData(localeIt);
 
 @NgModule({
   declarations: [
